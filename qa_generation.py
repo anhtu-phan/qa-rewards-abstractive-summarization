133ffd9caca9b57eaa40bf6c526d97ae0066622e
anhtu95 <anhtu23795@gmail.com>
2022-03-18 11:38:34 +0100

    add qa generation

---
+++ qa_generation.py
@@ -0,0 +1,180 @@
+'''
+https://github.com/patil-suraj/question_generation/blob/master/pipelines.py
+'''
+
+import torch
+from transformers import(
+    AutoModelForSeq2SeqLM, 
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
+import nltk
+from nltk import sent_tokenize
+import itertools
+nltk.download('punkt')
+
+
+class QAGeneration:
+    """Poor man's QG pipeline"""
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        ans_model: PreTrainedModel,
+        ans_tokenizer: PreTrainedTokenizer,
+        qg_format: str = 'highlight',
+        use_cuda: bool = True
+    ):
+        self.model = model
+        self.tokenizer = tokenizer
+
+        self.ans_model = ans_model
+        self.ans_tokenizer = ans_tokenizer
+
+        self.qg_format = qg_format
+
+        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+        self.model.to(self.device)
+
+        if self.ans_model is not self.model:
+            self.ans_model.to(self.device)
+
+        assert self.model.__class__.__name__ in ["T5ForConditionalGeneration", "BartForConditionalGeneration"]
+        
+        if "T5ForConditionalGeneration" in self.model.__class__.__name__:
+            self.model_type = "t5"
+        else:
+            self.model_type = "bart"
+
+    def __call__(self, inputs: str):
+        inputs = " ".join(inputs.split())
+        sents, answers = self._extract_answers(inputs)
+        flat_answers = list(itertools.chain(*answers))
+        
+        if len(flat_answers) == 0:
+          return []
+
+        if self.qg_format == "prepend":
+            qg_examples = self._prepare_inputs_for_qg_from_answers_prepend(inputs, answers)
+        else:
+            qg_examples = self._prepare_inputs_for_qg_from_answers_hl(sents, answers)
+        
+        qg_inputs = [example['source_text'] for example in qg_examples]
+        questions = self._generate_questions(qg_inputs)
+        output = [{'answer': example['answer'], 'question': que} for example, que in zip(qg_examples, questions)]
+        return output
+    
+    def _generate_questions(self, inputs):
+        inputs = self._tokenize(inputs, padding=True, truncation=True)
+        
+        outs = self.model.generate(
+            input_ids=inputs['input_ids'].to(self.device), 
+            attention_mask=inputs['attention_mask'].to(self.device), 
+            max_length=32,
+            num_beams=4,
+        )
+        
+        questions = [self.tokenizer.decode(ids, skip_special_tokens=True) for ids in outs]
+        return questions
+    
+    def _extract_answers(self, context):
+        sents, inputs = self._prepare_inputs_for_ans_extraction(context)
+        inputs = self._tokenize(inputs, padding=True, truncation=True)
+
+        outs = self.ans_model.generate(
+            input_ids=inputs['input_ids'].to(self.device), 
+            attention_mask=inputs['attention_mask'].to(self.device), 
+            max_length=32,
+        )
+        
+        dec = [self.ans_tokenizer.decode(ids, skip_special_tokens=False) for ids in outs]
+        answers = [item.split('<sep>') for item in dec]
+        answers = [i[:-1] for i in answers]
+        
+        return sents, answers
+    
+    def _tokenize(self,
+        inputs,
+        padding=True,
+        truncation=True,
+        add_special_tokens=True,
+        max_length=512
+    ):
+        inputs = self.tokenizer.batch_encode_plus(
+            inputs, 
+            max_length=max_length,
+            add_special_tokens=add_special_tokens,
+            truncation=truncation,
+            padding="max_length" if padding else False,
+            pad_to_max_length=padding,
+            return_tensors="pt"
+        )
+        return inputs
+    
+    def _prepare_inputs_for_ans_extraction(self, text):
+        sents = sent_tokenize(text)
+
+        inputs = []
+        for i in range(len(sents)):
+            source_text = "extract answers:"
+            for j, sent in enumerate(sents):
+                if i == j:
+                    sent = "<hl> %s <hl>" % sent
+                source_text = "%s %s" % (source_text, sent)
+                source_text = source_text.strip()
+            
+            if self.model_type == "t5":
+                source_text = source_text + " </s>"
+            inputs.append(source_text)
+
+        return sents, inputs
+    
+    def _prepare_inputs_for_qg_from_answers_hl(self, sents, answers):
+        inputs = []
+        for i, answer in enumerate(answers):
+            if len(answer) == 0: continue
+            for answer_text in answer:
+                sent = sents[i]
+                sents_copy = sents[:]
+                
+                answer_text = answer_text.strip()
+                
+                ans_start_idx = sent.find(answer_text)
+                
+                sent = f"{sent[:ans_start_idx]} <hl> {answer_text} <hl> {sent[ans_start_idx + len(answer_text): ]}"
+                sents_copy[i] = sent
+                
+                source_text = " ".join(sents_copy)
+                source_text = f"generate question: {source_text}" 
+                if self.model_type == "t5":
+                    source_text = source_text + " </s>"
+                
+                inputs.append({"answer": answer_text, "source_text": source_text})
+        
+        return inputs
+    
+    def _prepare_inputs_for_qg_from_answers_prepend(self, context, answers):
+        flat_answers = list(itertools.chain(*answers))
+        examples = []
+        for answer in flat_answers:
+            source_text = f"answer: {answer} context: {context}"
+            if self.model_type == "t5":
+                source_text = source_text + " </s>"
+            
+            examples.append({"answer": answer, "source_text": source_text})
+        return examples
+
+
+if __name__ == "__main__":
+    qa_tokenizer = AutoTokenizer.from_pretrained("valhalla/t5-base-qg-hl")
+    qa_model = AutoModelForSeq2SeqLM.from_pretrained("valhalla/t5-base-qg-hl")
+    ans_tokenizer = AutoTokenizer.from_pretrained("valhalla/t5-small-qa-qg-hl")
+    ans_model = AutoModelForSeq2SeqLM.from_pretrained("valhalla/t5-small-qa-qg-hl")
+    qa_gen = QAGeneration(model=qa_model, tokenizer=qa_tokenizer, ans_model=ans_model, ans_tokenizer=ans_tokenizer)
+    text4 = "Forrest Gump is a 1994 American comedy-drama film directed by Robert Zemeckis and written by Eric Roth. \
+        It is based on the 1986 novel of the same name by Winston Groom and stars Tom Hanks, Robin Wright, Gary Sinise, \
+        Mykelti Williamson and Sally Field. The story depicts several decades in the life of Forrest Gump (Hanks), \
+        a slow-witted but kind-hearted man from Alabama who witnesses and unwittingly influences several defining \
+        historical events in the 20th century United States. The film differs substantially from the novel."
+    print(qa_gen(text4))
--- training.py
+++ training.py
@@ -1,30 +1,97 @@
-from unittest import skip
-from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+from transformers import (
+    PegasusForConditionalGeneration, 
+    PegasusTokenizer, 
+    AutoTokenizer, 
+    AutoModelForQuestionAnswering, 
+    AutoModelForSeq2SeqLM,
+)
+from datasets import load_dataset
 import torch
-from datasets import load_dataset, load_dataset_builder
+from trl.ppo import PPOTrainer
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
+import time
+import os
+
+from qa_generation import QAGeneration
+
+batch_size = 256
+steps = 25600
+forward_batch_size = 16
+xsum_path = "./datasets/xsum.csv"
+max_token_len = 512
+
+summary_model_name = "google/pegasus-xsum"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+    
+summary_model = PegasusForConditionalGeneration.from_pretrained(summary_model_name).to(device)
+summary_model_ref = PegasusForConditionalGeneration.from_pretrained(summary_model_name).to(device)
+qa_tokenizer = AutoTokenizer.from_pretrained("valhalla/t5-base-qg-hl")
+qa_model = AutoModelForSeq2SeqLM.from_pretrained("valhalla/t5-base-qg-hl").to(device)
+gen_answer_tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
+gen_answer_model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad").to(device)
 
 
 def prepare_data():
-    dataset_builder = load_dataset_builder('xsum')
-    print(dataset_builder.cache_dir)
-    print(dataset_builder.info.features)
-    print(dataset_builder.info.splits)
+    ds = load_dataset("xsum", split="train")
+    # print(ds[0])
+    # print(ds.shape)
+    # print(ds.features)
+    ds.set_format('pandas')
+    df = ds[:]
+    return df
 
 
-def generate_summary():
-    src_text = [
-        """ PG&E stated it scheduled the blackouts in response to forecasts for high winds amid dry conditions. The aim is to reduce the risk of wildfires. Nearly 800 thousand customers were scheduled to be affected by the shutoffs which were expected to last through at least midday tomorrow."""
-    ]
+def reward_calculation(generated_summaries, ground_truth_summaries):
+    # generated_summaries_tokens = [qa_tokenizer.encode(sum, return_tensors='pt').to(device)[0, :] for sum in generated_summaries]
+    # generated_summaries_tokens = t5_tokenizer(generated_summaries).input_ids
+    qa_gen = QAGeneration(model=qa_model, tokenizer=qa_tokenizer, ans_model=qa_model, ans_tokenizer=qa_tokenizer)
+    print(qa_gen(generated_summaries[1]))    
 
-    model_name = "google/pegasus-xsum"
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(device)
-    tokenizer = PegasusTokenizer.from_pretrained(model_name)
-    model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
-    batch = tokenizer(src_text, truncation=True, padding="longest", return_tensors="pt").to(device)
-    translated = model.generate(**batch)
-    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-    print(tgt_text)
 
+def main():
+    if os.path.exists(xsum_path):
+        df = pd.read_csv(xsum_path)
+    else:
+        df = prepare_data()
+        summary_tokenizer = PegasusTokenizer.from_pretrained(summary_model_name)
+        #TO-DO increase length of tokens
+        df['tokens'] = df['document'].progress_apply(lambda x: summary_tokenizer.encode(x, return_tensors="pt", truncation=True, padding=True).to(device)[0, :])
+        print(df.head(1))
+        print("-------------------")
+        # print(df[0, :])
+        # print(df['tokens'][0].length)
+        df['query'] = df['tokens'].progress_apply(lambda x: summary_tokenizer.decode(x))
+        df.to_csv(xsum_path)
+        print(df.head(1))
+
+    ppo_trainer = PPOTrainer(summary_model, summary_model_ref)
+    for epoch in tqdm(range(int(steps/batch_size))):
+        torch.cuda.empty_cache()
+        logs = dict()
+        game_data = dict()
+        timing = dict()
+        t0 = time.time()
+
+        df_batch = df.sample(batch_size)
+        game_data['query'] = df_batch['query'].tolist()
+        query_tensors = torch.stack(df_batch['tokens'].tolist())
+        print(f'query_tensors shape ', query_tensors.shape)
+
+        t = time.time()
+        response_tensors = []
+        for i in range(int(batch_size/forward_batch_size)):
+            response = summary_model.generate(query_tensors)
+            response_tensors.append(response)
+        
+        print(f'response_tensors shape ', response_tensors.shape)
+        print(response_tensors)
+        response_tensors = torch.cat(response_tensors)
+        game_data['response'] = [summary_tokenizer.decode(response_tensors[i, :]) for i in range(batch_size)]
+        timing['time/get_response'] = time.time() - t
+        
+        
 if __name__ == "__main__":
-    prepare_data()
+    reward_calculation(["summarize: studies have shown that owning a dog is good for you", "42 is the answer to life, universe and everything."], [])
