6f82072e4eed3f6e14759bd3c0cd1421dcff2827
anhtu95 <anhtu23795@gmail.com>
2022-03-24 17:31:35 +0100

    fix resume_from_checkpoint keyword

--- finetuning/run_clm.py
+++ finetuning/run_clm.py
@@ -488,9 +488,9 @@ def main():
     # Training
     if training_args.do_train:
         checkpoint = None
-        if training_args.resume_from_checkpoint is not None:
-            checkpoint = training_args.resume_from_checkpoint
-        elif last_checkpoint is not None:
+        # if training_args.resume_from_checkpoint is not None:
+        #     checkpoint = training_args.resume_from_checkpoint
+        if last_checkpoint is not None:
             checkpoint = last_checkpoint
         train_result = trainer.train(resume_from_checkpoint=checkpoint)
         trainer.save_model()  # Saves the tokenizer too for easy upload
