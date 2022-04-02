040cc9c33991edf476de3abf19644e4d8b7db2cf
anhtu95 <anhtu23795@gmail.com>
2022-04-02 18:35:45 +0200

    modify submit with gpu

--- submit.sh
+++ submit.sh
@@ -3,8 +3,9 @@
 #SBATCH --job-name=qa_training
 #SBATCH --output=qa_training_output.txt
 #SBATCH --ntasks=1
-#SBATCH --time=1500:00
-#SBATCH --mem=8192
+#SBATCH --partition=students
+#SBATCH --time=1-0:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --mail-user=anhtu@cl.uni-heidelberg.de
 #SBATCH --mail-type=ALL
 
